10e949ec615670026d8f13b7d5fee82187348621
vinay adaki <vinay.adki123@gmail.com>
2021-04-04 13:59:01 +0530

    initial: Initial commit (#1)

---
+++ src/main/java/com/bits/OSSEAssignment4/OsseAssignment4Application.java
@@ -0,0 +1,13 @@
+package com.bits.OSSEAssignment4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsseAssignment4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsseAssignment4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bits/OSSEAssignment4/OsseAssignment4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.OSSEAssignment4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsseAssignment4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
